bb4719f182fa37f9d0d54ebc7ea9e4a87a83c47b
VENDEVILLE Antoine <a.vendeville@orange.fr>
2018-09-21 08:44:48 +0200

    feat(Web): Add material angular

--- rss-feeds-reader/src/app/app.module.ts
+++ rss-feeds-reader/src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { ActivatedRoute, Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
+import { MaterialModule } from './material.module';
+import { HttpClientModule } from '@angular/common/http';
+
+export const routes: Routes = [
+  { path: '',   redirectTo: '/information', pathMatch: 'full' }
+];
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    MaterialModule,
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { 
+  
+}
